0f4c0276a900f4f183712f79a8c93acc09ae8f48
NicholasQ <quan.nicholas@gmail.com>
2020-12-08 22:55:52 -0500

    Clean up day 7 pt 2

--- day7p2.py
+++ day7p2.py
@@ -4,15 +4,13 @@ with open("inputs/input7.txt", "r") as f:
 bag_index = {}
 
 def count_subbags(bag, bag_index):
-
-
     total = 0
+    
     if bag:
         total += sum(bag_index[bag][1])
         subbags = bag_index[bag]
 
         for i in range(len(subbags[0])):
-#           print(type(subbags[0]))
             total += count_subbags(subbags[0][i], bag_index) * subbags[1][i]
     
     return total
@@ -24,7 +22,6 @@ def form_bag_names(words):
             return_list += [words[i] + " " + words[i + 1]]
     return return_list
     
-
 for r in rules:
     raw_words = r.split()
     words = [w for w in raw_words if not w in "0123456789 contain bags, bags. bag. bag,"]
@@ -34,9 +31,6 @@ for r in rules:
     subbags = [bag for bag in form_bag_names(words[2:]) if bag != "no other"]
     bag_index[holder] = (subbags, numbers)
 
-count = 0
-
 golden_subbags = count_subbags("shiny gold", bag_index)
-
 print(golden_subbags)
 
