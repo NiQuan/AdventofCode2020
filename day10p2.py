e088141f17cca4448dc4e325a916d9cdae59a832
NicholasQ <quan.nicholas@gmail.com>
2020-12-11 19:15:47 -0500

    Remove math library

--- day10p2.py
+++ day10p2.py
@@ -1,5 +1,3 @@
-import math
-
 with open("inputs/input10.txt", "r") as f:
     joltages = [0] + [int(n) for n in f.read().splitlines()] #Adding the 0 for the source
 
@@ -16,16 +14,12 @@ for gap in joltage_gaps:
         one_streak_lens += [one_streak_ct]
         one_streak_ct = 0
 
-print(joltages)
-print(joltage_gaps)
-print(one_streak_lens)
-
 acc = 1
 for i in one_streak_lens:
     if 0 < i <= 3:
         acc = acc * (2 ** (i - 1))
     elif i == 4:
-        acc = acc * 7
+        acc = acc * 7 # 7 is 2**8 minus 1
     elif i > 4:
         raise (i)
 
