58a03f830179df1931bb333807b4c8ae450c4d27
NicholasQ <quan.nicholas@gmail.com>
2020-12-26 19:58:34 -0500

    Lint

--- day15.py
+++ day15.py
@@ -1,11 +1,12 @@
 with open("inputs/input15.txt", "r") as f:
-    start_nums = [int(c) for c in f.read().split(',')]
+    start_nums = [int(c) for c in f.read().split(",")]
 
 hist_dict = {}
 count = 1
-for n in start_nums: # Initialize the history dict
+for n in start_nums:  # Initialize the history dict
     hist_dict[n] = count
     count += 1
+
 
 def next_num(d, recent, t):
     last_t = d.get(recent)
@@ -14,6 +15,7 @@ def next_num(d, recent, t):
         return t - last_t
     else:
         return 0
+
 
 start = len(start_nums)
 recent = start_nums[-1]
