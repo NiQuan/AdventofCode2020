801fc9193b6ef8fdd81269f51a1beaf535ed85f7
NicholasQ <quan.nicholas@gmail.com>
2020-12-30 23:39:35 -0500

    Finish Day 19

--- days/day19.py
+++ days/day19.py
@@ -36,7 +36,7 @@ def str_combinations(list1, list2):
         return combs
 
 
-def all_valid(r):
+def all_valid_p1(r):
     if not r:
         return None
     elif r[6]:
@@ -45,16 +45,40 @@ def all_valid(r):
     valid_lst = []
     subs = subrules(r)
 
-    valid_lst += str_combinations(all_valid(subs[0]), all_valid(subs[1]))
+    valid_lst += str_combinations(all_valid_p1(subs[0]), all_valid_p1(subs[1]))
     if subs[2]:
-        valid_lst += str_combinations(all_valid(subs[2]), all_valid(subs[3]))
+        valid_lst += str_combinations(all_valid_p1(subs[2]), all_valid_p1(subs[3]))
 
     return valid_lst
 
 
+def valid_p2(r):
+    matches_42 = all_valid_p1(rule_dict["42"])
+    matches_31 = all_valid_p1(rule_dict["31"])
+
+    # Track if we met the requirements of the first and 2nd halves
+    first_half = False
+    second_half = False
+    count_42 = count_31 = 0
+
+    for i in range(0, len(r), 8):
+        if r[i : i + 8] in matches_42 and not second_half:
+            first_half = True
+            count_42 += 1
+        elif r[i : i + 8] in matches_31 and first_half:
+            second_half = True
+            count_31 += 1
+        else:
+            return False
+
+    return second_half and (count_31 < count_42)
+
+
 main_rule = rule_dict["0"]
-valids = all_valid(main_rule)
+valids = all_valid_p1(main_rule)
 
-count = len([m for m in messages if m in valids])
+count_p1 = len([m for m in messages if m in valids])
+count_p2 = len([m for m in messages if valid_p2(m)])
 
-print(count)
+print(count_p1)
+print(count_p2)
