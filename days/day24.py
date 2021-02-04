2623a9e15abbb00d5874d096d6b37ba35915a55e
NicholasQ <quan.nicholas@gmail.com>
2021-02-04 00:02:28 -0500

    Add day 24 part 1

---
+++ days/day24.py
@@ -0,0 +1,73 @@
+with open("inputs/input24.txt", "r") as f:
+    paths = f.read().splitlines()
+
+
+def parse_path(p):
+    parsed = []
+    p_lst = list(p)
+    while len(p_lst) != 0:
+        char_1 = p_lst.pop(0)
+        if char_1 in "ns":
+            char_2 = p_lst.pop(0)
+            parsed.append(char_1 + char_2)
+        else:
+            parsed.append(char_1)
+    return parsed
+
+
+def to_cartesian(p):
+    n = 0
+    e = 0
+
+    for move in p:
+        if move == "e":
+            e += 2
+        elif move == "w":
+            e -= 2
+        else:
+            if "n" in move:
+                n += 1
+            else:
+                n -= 1
+
+            if "e" in move:
+                e += 1
+            else:
+                e -= 1
+
+    return (n, e)
+
+
+def neighbor_coords(coord):
+    (n,e) = coord
+    ns = [(n, e+2), (n, e-2), (n+1, e+1), (n-1, e+1), (n+1, e-1), (n-1, e-1))]
+    return ns
+
+def count_blacks(coords, black_tiles):
+    count = 0
+    for c in coords:
+        if c in black_tiles:
+            count += 1
+    return count
+
+def all_possible(black_tiles):
+    total_set = set()
+    for t in black_tiles:
+        ns = neighbor_coords(t)
+        for n in ns:
+            total_set.add(n)
+        total_set.add(t)
+    return total_set
+
+cartesians = [to_cartesian(parse_path(p)) for p in paths]
+black_tiles = set()
+
+for c in cartesians:
+    if c in black_tiles:
+        black_tiles.remove(c)
+    else:
+        black_tiles.add(c)
+
+print(len(black_tiles))
+
+print(cartesians)
